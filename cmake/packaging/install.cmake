699530fba1ebef98141bec42149cbaadccd4dde0
Diego Stocco <diego.stocco@cern.ch>
2025-08-04 15:28:40 +0200

    CMake utilities to package the project

---
+++ cmake/packaging/install.cmake
@@ -0,0 +1,24 @@
+include_guard()
+
+set(PREMDIR ${CMAKE_INSTALL_PREFIX}/PremTables)
+set(MODEL3DDIR ${CMAKE_INSTALL_PREFIX}/EarthTables)
+set(PREMFILE ${PREMDIR}/prem_default.txt)
+set(PREM3DFILE ${MODEL3DDIR}/earth_binned_default.txt)
+
+configure_file(
+  ${CMAKE_CURRENT_LIST_DIR}/prem_default.hpp.in
+  ${CMAKE_BINARY_DIR}/prem_default.hpp @ONLY USE_SOURCE_PERMISSIONS)
+
+install(DIRECTORY ${PROJECT_SOURCE_DIR}/PremTables
+        DESTINATION ${CMAKE_INSTALL_PREFIX})
+
+install(DIRECTORY ${PROJECT_SOURCE_DIR}/EarthTables
+        DESTINATION ${CMAKE_INSTALL_PREFIX})
+
+install(
+  EXPORT oscprob
+  DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/oscprob
+  NAMESPACE OscProb::)
+
+install(FILES ${CMAKE_CURRENT_LIST_DIR}/oscprob-config.cmake
+        DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/oscprob)
